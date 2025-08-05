58797f4675e4acd30d8146cfb1ff942dad0a8ebf
Ciro Lucio Tecce <cirolucio.tecce@gmail.com>
2025-08-05 16:45:04 +0200

    refactor: change get_all_file_paths to generator using os.walk and add type hints

--- src/pylocc/file_utils.py
+++ src/pylocc/file_utils.py
@@ -1,33 +1,23 @@
 
 from pathlib import Path
-from typing import List
+from typing import Iterator, List
+import os
 
-def get_all_file_paths(folder: str, supported_extensions: List[str] = []) -> List[str]:
-    """
-    Returns a list of all file paths.
-
-    Args:
-        folder (str): Path to the root folder to search
-
-    Returns:
-        List[str]: List of absolute file paths as strings
-    """
+def get_all_file_paths(folder: str, supported_extensions: List[str] = [])-> Iterator[str]:
     folder_path = Path(folder)
-
     if not folder_path.exists():
         raise FileNotFoundError(f"The path '{folder_path}' does not exist")
     if not folder_path.is_dir():
         raise NotADirectoryError(
             f"The path '{folder_path}' is not a directory")
 
-    # Use rglob to recursively find all files
-    file_paths = [str(file.resolve())
-                  for file in folder_path.rglob('*') if file.is_file()]
-
-    # Filter by supported extensions if provided
+    extensions_set = None
     if supported_extensions:
         extensions_set = set(supported_extensions)
-        file_paths = [file for file in file_paths if Path(
-            file).suffix[1:] in extensions_set]
 
-    return file_paths
+    for root, _, files in os.walk(folder):
+        for file in files:
+            if not extensions_set or Path(file).suffix[1:] in extensions_set:
+                # return the path
+                yield os.path.join(root, file)
+    return None
